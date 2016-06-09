3f7aeb72fa2281798394f0a248acaa3db1c0426c
Charles Du <charles.q.du@theprolog.com>
2016-06-09 18:00:22 +0800

    switched to using Sequel, took a while to figure out need to use create_table? instead of create_table, trick for setting column to Array of Strings did not work

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
   gem 'sinatra'
-  gem 'dm-sqlite-adapter'
-  gem 'data_mapper'
+  gem 'sequel'
   gem 'rake'
+  gem 'haml'
--- sinatratest.rb
+++ sinatratest.rb
@@ -1,64 +1,66 @@
 require 'rubygems'
 require 'sinatra'
-require 'data_mapper'
+require 'sequel'
+require 'sqlite3'
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/articles.db")
- 
-class Article < Sinatra::Base
-  include DataMapper::Resource
-  property :id, Serial
-  property :author_name, Text, :required => true
-  property :body, Text, :required => true
-  property :title, Text, :required => true
-  property :keywords, Text
-  property :image_url, Text
-  property :created_at, DateTime
-  property :updated_at, DateTime
+DB = Sequel.connect('sqlite://articles.db')
+
+DB.create_table? :articles do
+  primary_key :id
+  String :author_name, :default=>"User1"
+  String :body
+  String :title
+  String :keywords, :null=>true
+  String  :image_url, :null=>true
+  DateTime :created_at
+  DateTime :updated_at
+end
+
+class Article < Sequel::Model
 end
  
-DataMapper.finalize.auto_upgrade!
-
 get '/' do
-  @articles = Article.all :order => :id.desc
+  @articles = Article.all
   @title = 'All Articles'
   erb :home
 end
 
 post '/' do
-  n = Article.new
-  n.body = params[:body]
-  n.title = params[:title]
-  n.author_name = "User1"
-  n.keywords = params[:keywords]
-  n.image_url = params[:image_url]
-  n.created_at = Time.now
-  n.updated_at = Time.now
+  n = Article.new do |n|
+    n.body = params[:body]
+    n.title = params[:title]
+    n.author_name = "User1"
+    n.keywords = params[:keywords]
+    n.image_url = params[:image_url]
+    n.created_at = Time.now
+    n.updated_at = Time.now
+  end
   n.save
   redirect '/'
 end
 
-get '/:id' do
-  @article = Article.get params[:id]
-  @title = "Edit article ##{params[:id]}"
+get '/:id' do |id|
+  @article = Article[id]
+  @title = "Edit article id"
   erb :edit
 end
 
-put '/:id' do
-  n = Article.get params[:id]
+put '/:id' do |id|
+  n = Article[id]
   n.body = params[:body]
   n.updated_at = Time.now
   n.save
   redirect '/'
 end
 
-get '/:id/delete' do
-  @article = Article.get params[:id]
-  @title = "Confirm deletion of article ##{params[:id]}"
+get '/:id/delete' do |id|
+  @article = Article[id]
+  @title = "Confirm deletion of article ##id"
   erb :delete
 end
 
-delete '/:id' do
-  n = Article.get params[:id]
+delete '/:id' do |id|
+  n = Article[id]
   n.destroy
   redirect '/'
 end
