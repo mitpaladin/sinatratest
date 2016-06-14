57ff6e63c02a36d9898538a5587480d23a5cfd2a
Charles Du <charles.q.du@theprolog.com>
2016-06-15 00:20:36 +0800

    fixed Procfile used by foreman

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: bundle exec ruby test.rb  
-guard: bundle exec guard -i  
+web: bundle exec ruby spec/*_spec.rb  
--- spec/sinatratest_spec.rb
+++ spec/sinatratest_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
   describe "my app" do
     it "should return a UI" do
