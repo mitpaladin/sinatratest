fe048d1a48ecc52729a17969c17975f7c79ab926
Charles Du <charles.q.du@theprolog.com>
2016-06-16 19:09:33 +0800

    UI now storing keywords as Strings but converts to array before rending through SummariseContent

--- lib/sinatratest.rb
+++ lib/sinatratest.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sequel'
 require 'sqlite3'
+require 'prolog/use_cases/summarise_content'
 
 DB = Sequel.connect('sqlite://articles.db')
 
@@ -16,18 +17,45 @@ DB.create_table? :articles do
   DateTime :updated_at
 end
 
-# Sequel model based article
-class Article < Sequel::Model
+# Sequel model based articles
+class Articles < Sequel::Model
+end
+
+# Intermediate repo object with keywords as array rather than String
+class ArticleRepo
+  def initialize(articles)
+    @articles = articles
+    keywords_to_a
+    self
+  end
+
+  def all
+    @articles
+  end
+
+  def keywords_to_a
+    @articles.each do |art|
+      oldkeywords = art.keywords
+      art.keywords = oldkeywords.split(/,/) || []
+    end
+  end
+
+  private
+
+  attr_reader :articles
 end
 
 get '/' do
-  @articles = Article.all
+  artrepo = ArticleRepo.new(Articles.all)
+  sumobj = Prolog::UseCases::SummariseContent.new(repository: artrepo)
+  rethash = sumobj.call
+  @articles = rethash[:articles]
   @title = 'All Articles'
   erb :home
 end
 
 post '/' do
-  newart = Article.new do |n|
+  newart = Articles.new do |n|
     n.body = params[:body]
     n.title = params[:title]
     n.author_name = 'User1'
@@ -41,13 +69,13 @@ post '/' do
 end
 
 get '/:id' do |id|
-  @article = Article[id]
+  @article = Articles[id]
   @title = 'Edit article id'
   erb :edit
 end
 
 put '/:id' do |id|
-  n = Article[id]
+  n = Articles[id]
   n.body = params[:body]
   n.updated_at = Time.now
   n.save
@@ -55,13 +83,13 @@ put '/:id' do |id|
 end
 
 get '/:id/delete' do |id|
-  @article = Article[id]
+  @article = Articles[id]
   @title = 'Confirm deletion of article ##id'
   erb :delete
 end
 
 delete '/:id' do |id|
-  n = Article[id]
+  n = Articles[id]
   n.destroy
   redirect '/'
 end
